8c92608c28d860c500b8a679e9360c54a3bc2e30
milan400 <tripathimilan28@gmail.com>
2020-10-07 15:25:10 +0545

    first commit

---
+++ lambda_function.py
@@ -0,0 +1,29 @@
+import requests, json
+
+
+def lambda_handler(event, context):
+
+	entity = event["currentIntent"]["slots"]["location"].title()
+
+	api_key = "1e2992f2efaf0cfa567331c246ccc4db"
+	base_url = "http://api.openweathermap.org/data/2.5/weather?"
+
+	city_name = entity
+	
+	complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+
+	response = requests.get(complete_url)
+	x = response.json()
+	weather = x['weather'][0]['description']
+
+	return{
+        "sessionAttributes": event['sessionAttributes'],
+        "dialogAction":{
+            "type":"Close",
+            "fulfillmentState": "Fulfilled",
+            "message":{
+                "contentType":"PlainText",
+                "content": entity
+            }
+        }
+    }
