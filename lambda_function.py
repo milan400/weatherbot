8c515508c5f0cf89aaaa13d922fb8e818e69d188
milan <tripathimilan28@gmail.com>
2020-10-09 16:02:22 +0545

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -2,28 +2,30 @@ import requests, json
 
 
 def lambda_handler(event, context):
+    
+    if(event["currentIntent"]["name"]=='TodaysWeather'):
 
-	entity = event["currentIntent"]["slots"]["location"].title()
-
-	api_key = "1e2992f2efaf0cfa567331c246ccc4db"
-	base_url = "http://api.openweathermap.org/data/2.5/weather?"
-
-	city_name = entity
-	
-	complete_url = base_url + "appid=" + api_key + "&q=" + city_name
-
-	response = requests.get(complete_url)
-	x = response.json()
-	weather = x['weather'][0]['description']
-
-	return{
-        "sessionAttributes": event['sessionAttributes'],
-        "dialogAction":{
-            "type":"Close",
-            "fulfillmentState": "Fulfilled",
-            "message":{
-                "contentType":"PlainText",
-                "content": entity
+    	entity = event["currentIntent"]["slots"]["location"].title()
+    
+    	api_key = "1e2992f2efaf0cfa567331c246ccc4db"
+    	base_url = "http://api.openweathermap.org/data/2.5/weather?"
+    
+    	city_name = entity
+    	
+    	complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+    
+    	response = requests.get(complete_url)
+    	x = response.json()
+    	weather = x['weather'][0]['description']
+    
+    	return{
+            "sessionAttributes": event['sessionAttributes'],
+            "dialogAction":{
+                "type":"Close",
+                "fulfillmentState": "Fulfilled",
+                "message":{
+                    "contentType":"PlainText",
+                    "content": weather
+                }
             }
         }
-    }
